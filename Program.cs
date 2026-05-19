514a83dba42c589d50b33999db2a6775ab2eeafd
jdcast <jdcast072@gmail.com>
2026-05-19 02:30:21 -0500

    feat: args cli, stdin & stdout; program interactive

--- Program.cs
+++ Program.cs
@@ -3,7 +3,36 @@
 var assembly = Assembly.GetExecutingAssembly();
 var version = assembly.GetName().Version;
 
-Console.WriteLine("Hello, World!");
+if (args.Length > 0){
+    switch (args[0].ToLower())
+    {
+        case "--help":
+            mostrarAyuda();
+            Environment.Exit(0);
+            break;
+        case "--version":
+            Console.WriteLine($"Versión de InventarioApp v{version}");
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine($"Error, opción desconocida: {args[0]}");
+            Console.WriteLine($"Use --help para ver las opciones disponibles.");
+            Environment.Exit(2);
+            break;
+    };
+};
+
+mostrarBanner();
+
+Console.Write("Ingrese un comando o (use 'salir' para terminar): ");
+string? entrada = Console.ReadLine(); //Stdin: Lee la entrada del usuario
+
+if (string.IsNullOrWhiteSpace(entrada) || entrada.ToLower() == "salir") {
+    Console.WriteLine("Hasta luego. Saliendo... ");//Stdout: Salida del comando o programa
+    Environment.Exit(0);
+}
+
+void mostrarBanner() {
 Console.WriteLine("╔══════════════════════════════════════╗");
 Console.WriteLine("║   SISTEMA DE GESTIÓN DE INVENTARIO   ║");
 Console.WriteLine("╚══════════════════════════════════════╝");
@@ -12,6 +41,28 @@ Console.WriteLine($"Versión del programa: {version}");
 Console.WriteLine($".NET version: {Environment.Version}");
 Console.WriteLine($"Plataforma del SO: {Environment.OSVersion.Platform}");
 Console.WriteLine($"Nombre ejecutado del programa: {assembly}");
+}
+
+// Modo interactgivo si no hay args
+// Write: Sirve para mostrar un mensaje al usuario sin saltos de línea, ideal para prompts
+
+
+void mostrarAyuda() {
+    Console.WriteLine("Uso: inventarioApp [comando] [opciones]");
+    Console.WriteLine();
+    Console.WriteLine("Comandos:");
+    Console.WriteLine("  --help, -h    Muestra esta ayuda");
+    Console.WriteLine("  --version, -v    Muestra la versión del programa");
+    Console.WriteLine();
+    Console.WriteLine("Ejemplos:");
+    Console.WriteLine("  dotnet run -- --help");
+    Console.WriteLine("  dotnet run -- --version");
+    Console.WriteLine("Si no se proporcionan opciones, el programa se ejecutará en modo interactivo.");
+}
+
+
+
+/*
 Console.WriteLine("📁 Estructura:");
 Console.WriteLine("   ✓ Configuración .csproj");
 Console.WriteLine("   ✓ Estructura src/Models/");
@@ -22,3 +73,4 @@ Console.WriteLine("════════════════════�
 Console.WriteLine("  ✓ MÓDULO 1 COMPLETADO");
 Console.WriteLine("  → Siguiente: Módulo 2 - CLI interactiva");
 Console.WriteLine("═══════════════════════════════════════");
+*/
--- README.md
+++ README.md
@@ -12,6 +12,12 @@ InventarioApp/
 - InventarioApp. csproj # Configuracion
 - .gitignore # Archivos ignorados por Git
 - src/
+
+## `args`, `stdin` y `stdout` 
+
+Se modifica el código del archivo Program.cs para agregar una banner, condicional tipo interruptor (`switch`) y argumentos CLI para mostrar por **argumentos CLI** de flags de ayuda (`--help`) y la versión actual del proyecto (`--version`).
+Además, se agrega un modo interactivo de prueba con un prompt básico para salir de **InventarioApp**.
+
 ---
 ### Autor
 Juan Diego Castañeda
@@ -21,3 +27,4 @@ Sebastian Martinez
 
 ### Progreso del curso
 - [x] Módulo 1: Ecosistema .NET.
+- [x] Módulo 2: Entradas y salidas con args
